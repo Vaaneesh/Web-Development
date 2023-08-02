75a6f6fed67c7e85904dbce6e34e50ee94da90b9
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-08-02 19:27:21 +0530

    modified

--- file.js
+++ file.js
@@ -1,6 +1,73 @@
 // var let const
 const c=10;
 console.log(c);
-c=11;
+// c=11;
 console.log(c);
 //const: constant
+var e=100;
+console.log(e);
+
+//string
+let str=''
+let str1='A'
+let str2='yoo'
+let str3="sup"
+console.log(str)
+console.log(typeof str)
+console.log(str1)
+console.log(str2)
+console.log(str3)
+console.log(str)
+
+//number
+let n1=10
+let n2=10.11
+let n3=Infinity
+let n4=NaN
+
+console.log(typeof n1)
+console.log(n2)
+console.log(typeof n3)
+console.log(n3)
+console.log(n4)
+console.log(str)
+
+//boolean
+let b1=true
+let b2=false
+console.log(typeof b1)
+console.log(b1)
+console.log(b2)
+console.log(str);
+
+let a;
+console.log(a)
+console.log(typeof a)
+console.log(str);
+//functions
+function change(){
+    console.log("change");
+}
+console.log(typeof change);
+console.log(str);
+
+//object
+let f=null;
+console.log(typeof f);
+
+let arrI=[1,2,3];
+let arrB=[true,false,true];
+let arrF=[10.1,10.2,10.3];
+
+console.log(str);
+console.log(typeof arrI);
+console.log(typeof arrB);
+console.log(typeof arrF);
+
+console.log(str);
+
+for(let i=0;i<arrI.length;i++){
+    str+=arrI[i]+' ';
+}
+console.log(str);
+console.log(typeof str);
