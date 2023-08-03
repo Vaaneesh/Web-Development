bac61fef46a917402811864bf72093b6379fcc8e
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-08-03 18:53:12 +0530

    changes made

--- objects.js
+++ objects.js
@@ -4,3 +4,20 @@ let obj={
     key:"value",
     key2:"value2"
 }
+console.log(obj);
+
+let arr=[1,2,3];
+console.log(arr)
+
+//accessing values using dot notation
+let details={
+    "first":"Vaaneesh",
+    second:"Prabhakar"
+}
+//changing using dot notation
+details.first="Vasu"
+console.log(details.first)
+
+//deletion using dot notation
+delete details.first;
+console.log(details.first+' '+details.second)
