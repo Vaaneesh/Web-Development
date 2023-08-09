715625a14191edff213067766efe426dbc25e073
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-08-09 19:15:13 +0530

    changes made

--- function.js
+++ function.js
@@ -6,3 +6,16 @@ var sub=function(a,b){
     return a-b;
 }
 console.log(sub(10,20));
+
+var a=10;
+function foo(){
+    console.log(a);
+    var a=20;
+    function foo1(){
+        a++;
+        console.log(a);
+        var a=30;
+        console.log(a);
+    }
+}
+console.log(foo());
