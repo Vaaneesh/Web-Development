aa7957f2012d03e688a02de7fdc453f1b571db26
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-08-10 19:31:37 +0530

    modified

--- closures.js
+++ closures.js
@@ -1,17 +1,23 @@
 function outer(){
     var a=10;
     function inner(){
-        a++;
-        console.log(a);
+        var b=20;
+        function innermost(){
+            a++;
+            b++;
+            console.log(a);
+            console.log(b);
+        }
+        return innermost;
     }
     return inner;
 }
 let f1=outer();
-let f2=outer();
+let f2=f1();
 // console.log(f1);
-f1();
-f1();
-f1(); //means closure mei agr value change hogi toh maintain rhegi value. e.g- 11,12,13;
-f2();
+// f1();
+// f1();
+// f1(); //means closure mei agr value change hogi toh maintain rhegi value. e.g- 11,12,13;
+// f2();
 f2();
 f2();
