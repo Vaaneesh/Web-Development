2f6643648216d90cb33304bac0cef9823b43c5ce
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-08-21 17:03:10 +0530

    modified+file added

---
+++ dom2.js
@@ -0,0 +1,9 @@
+/*
+parentElement
+previousSibling
+*/
+let h3=document.querySelector("h3");
+console.dir(h3);
+let parent=h3.parentElement;
+console.log(parent);
+let h1=document.querySelector("h1");
