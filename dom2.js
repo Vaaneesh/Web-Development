f72bf798548b9fb6fc2eb46eeafc992ca97bcee4
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-08-21 17:17:19 +0530

    changes made

--- dom2.js
+++ dom2.js
@@ -7,3 +7,16 @@ console.dir(h3);
 let parent=h3.parentElement;
 console.log(parent);
 let h1=document.querySelector("h1");
+let grandparent=h1.parentElement.parentElement;
+console.log(grandparent);
+
+//prevSibling
+let prevsb=h1.previousElementSibling;
+console.log(prevsb);
+
+//nextSibling
+let nextsb=h1.nextElementSibling;
+console.log(nextsb);
+
+let h1parent=h1.parentElement.nextElementSibling;
+console.log(h1parent);
