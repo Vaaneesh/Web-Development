db6f3c9eaf090ebdf5fbf4634592cf94668fd0c9
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-08-24 17:41:47 +0530

    2 files added

---
+++ EventBubbling.js
@@ -0,0 +1,13 @@
+let Grandfather=document.querySelector(".Grandfather");
+let father=document.querySelector(".father");
+let mother=document.querySelector(".mother");
+
+Grandfather.addEventListener("click",function(){
+    console.log("Grandfather");
+})
+father.addEventListener("click",function(){
+    console.log("father");
+})
+mother.addEventListener("click",function(){
+    console.log("mother");
+})
