5247307061527a18b3612d3d3348e80c30e05b2b
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-08-24 17:21:14 +0530

    modified

--- keypress.js
+++ keypress.js
@@ -2,3 +2,21 @@ let input=document.querySelector("input");
 input.addEventListener("keypress",function(ev){
     console.log(ev.key);
 })
+
+let form=document.querySelector(".myform");
+console.log(form);
+form.addEventListener("submit",function(ev){
+    ev.preventDefault();
+    let inp=document.querySelector("#inp");
+    console.log(inp.value);
+    addElement2(inp.value);
+    // console.log(ev);
+})
+function addElement2(value){
+    let ul=document.querySelector("ul");
+    let li=document.createElement("li");
+    li.classList.add("listItem");
+    li.innerText=`${value}`;
+    // li.innerText=inp.value;
+    ul.append(li);
+}
