4c8d2aeff80e39b945504d01b88ff870985c773a
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-08-25 17:34:08 +0530

    changes made

--- to-do-class.js
+++ to-do-class.js
@@ -11,5 +11,21 @@ form.addEventListener("submit",function(ev){
 function Add(value){
     let ul=document.querySelector("ul");
     let li=document.createElement("li");
-    li.classList.add
+    li.classList.add("taskItem");
+    li.innerHTML=`${value} <span> <button class="delete">Delete</button></span>`;
+    ul.append(li);
 }
+
+//delete
+let taskList=document.querySelector(".taskList");
+taskList.addEventListener("click",function(ev){
+    // console.log(ev.target);
+    let currentElement=ev.target;
+    let className=ev.target.className;
+    console.log(className);
+    if(className=="delete"){
+        let item=currentElement.parentElement.parentElement;
+        console.log(item);
+        item.remove();
+    }
+})
