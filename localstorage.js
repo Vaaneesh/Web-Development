a8eaa59e8d82c6ee8c1767274a3bbfaccabfab61
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-08-31 17:42:49 +0530

    modified

---
+++ localstorage.js
@@ -0,0 +1,11 @@
+let products=[{
+    pname:"mobile",
+    pquant:20,
+    price:30000
+},{
+    pname:"Laptop",
+    pquant:20,
+    price:60000
+}]
+//add
+localStorage.setItem("products",products);
