03a9d4170c9cff62a53b6587f34385fbcaa12d40
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-09-05 16:48:55 +0530

    new folder created

---
+++ sync/script.js
@@ -0,0 +1,13 @@
+console.log("Hi");
+function wait(){
+    let currentTime=new Date().getTime();
+    while(currentTime+5000>new Date().getTime()){
+        
+    }
+};
+wait();
+console.log("Function be baad");
+sub(20,5);
+function sub(c,d){
+    console.log(c-d);
+}
