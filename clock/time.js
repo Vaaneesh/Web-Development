520ac2112b7ee08b6e7c1c5a9dce927c6e7df17a
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-09-06 14:49:14 +0530

    changes made

--- clock/time.js
+++ clock/time.js
@@ -1,5 +1,21 @@
-let h2=document.querySelector(".time");
-let time=new Date();
-time=time.toLocaleTimeString();
-h2.innerText=time;
-console.log(time.toLocaleString());
+// let h2=document.querySelector(".time");
+// let time=new Date();
+// time=time.toLocaleTimeString();
+// h2.innerText=time;
+// console.log(time.toLocaleString());
+function getTime(){
+    var date=new Date();
+    var hrs=date.getHours();
+    var min=date.getMinutes();
+    var sec=date.getSeconds();
+    if(hrs>12){
+        session.innerHTML="PM";
+    }
+    else{
+        session.innerHTML="AM";
+    }
+    document.getElementById('hours').innerHTML=hrs;
+    document.getElementById('minutes').innerHTML=min;
+    document.getElementById('sec').innerHTML=sec;
+}
+setInterval(getTime,10);
