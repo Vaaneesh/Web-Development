61d3fa9752a42825744cb68aad6487607c822e20
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-09-11 17:23:57 +0530

    file added

---
+++ Callback/script3.js
@@ -0,0 +1,29 @@
+function download(URL,cb){
+    if(!URL.startsWith("http")){
+        throw new Error("invalid URL");
+    }
+    setTimeout(() => {
+        let downloadedFile=URL.split("/").pop();
+        console.log(downloadedFile);
+        cb(downloadedFile);
+    }, 3000);
+}
+function Compress(file,cb){//movie.mp4-->movie.zip
+    setTimeout(()=>{
+        let zippedfile=file.split(".")[0]+".zip";
+        console.log(zippedfile);
+        cb(zippedfile);
+    },2000)
+}
+function upload(zip,cb){
+    let newURL="http://chitkara.com/"+zip;
+    setTimeout(() => {
+        console.log("Uploaded at new URL=> "+newURL);
+    }, 1000);
+}
+// download("http://chitkara.com/movie.mp4");
+download("http://chitkara.com/movie.mp4",function(downloadedFile){
+    Compress(downloadedFile,function(zippedfile){
+        upload(zippedfile,function(){})
+    })
+});
