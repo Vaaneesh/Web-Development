348b55a4038e38e53ca1c31f6669a1ec88f68566
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-09-12 17:19:18 +0530

    modified

--- Promise/script.js
+++ Promise/script.js
@@ -1,19 +1,25 @@
 //create
 function kalsepinaband(){
     return new Promise(function(resolve,reject){
-        resolve("Thik hai bhai chodh di");
+        let accountBalance=12000;
+        if(accountBalance<10000){
+            resolve("Thik hai bhai chodh di");
+        }
+        else{
+            reject("control kr bhai");
+        }
     })
     ;
 }
 //consume
 //1.then
 let p=kalsepinaband();
-console.log(p);
+// console.log(p);
 p.then(
-    function(){
-        
+    function(msg){
+        console.log(msg);
     },
-    function(){
-
+    function(msg){
+        console.log(msg);
     }
 )
