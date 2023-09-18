a1d8b3d2e9042e79e1f632ce5a3475ca8dcdab9f
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-09-18 17:19:13 +0530

    modified

--- Promise/promiseChange.js
+++ Promise/promiseChange.js
@@ -1,5 +1,8 @@
-function chopping(veg){
+function chopping(veg,quality){
     return new Promise((resolve,reject)=>{
+        if(quality<50){
+            return reject("Lode veges jada de!!");
+        }
         setTimeout(() => {
             let choppedVeg=veg+" chopped ";
             resolve(choppedVeg+"hori bhut bayankr");
@@ -8,14 +11,19 @@ function chopping(veg){
 }
 function cooking(choppedVeg){
     return new Promise((resolve, reject) => {
+        if(true){
+            return reject("Cooking nhi hogi!!");
+        }
         setTimeout(() => {
             resolve("Cooking hori bhut bayankr");
         }, 2000);  
     })
 }
-chopping("Potato").then((data) => {
+chopping("Potato",30).then((data) => {
     console.log(data);
     return cooking(data);
 }).then((data)=>{
     console.log(data);
-})
+}).catch((err)=>{
+    console.log(err);
+});
