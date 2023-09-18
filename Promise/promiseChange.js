8cd058f059c509ef15a5c088425c56e12efadc84
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-09-18 17:42:15 +0530

    modified

--- Promise/promiseChange.js
+++ Promise/promiseChange.js
@@ -11,19 +11,43 @@ function chopping(veg,quality){
 }
 function cooking(choppedVeg){
     return new Promise((resolve, reject) => {
-        if(true){
-            return reject("Cooking nhi hogi!!");
-        }
+        // if(true){
+        //     return reject("Cooking nhi hogi!!");
+        // }
         setTimeout(() => {
             resolve("Cooking hori bhut bayankr");
         }, 2000);  
     })
 }
-chopping("Potato",30).then((data) => {
+// 1) sync
+//chopping("Potato",30).then((data) => {
+//     console.log(data);
+//     return cooking(data);
+// }).then((data)=>{
+//     console.log(data);
+// }).catch((err)=>{
+//     console.log(err);
+// });
+
+//2)async /await
+async function myTask(){
+    let data=await chopping("onion",75);  //await is always used with async func
     console.log(data);
-    return cooking(data);
-}).then((data)=>{
-    console.log(data);
-}).catch((err)=>{
-    console.log(err);
-});
+    let data2=await cooking(data);
+    console.log(data2);
+}
+myTask();
+
+//3) try{}catch(){}block
+async function myTask(){
+    try{
+        let data=await chopping("onion",75);  //await is always used with async func
+        console.log(data);
+        let data2=await cooking(data);
+        console.log(data2);
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+myTask();
