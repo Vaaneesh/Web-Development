1fbbb77cb273afb02b70154f041af3698ae88977
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-09-19 17:18:09 +0530

    modified

--- Fetch/script.js
+++ Fetch/script.js
@@ -1,15 +1,17 @@
 let url="https://jsonplaceholder.typicode.com/todos/1";
-fetch(url).then((data)=>{
-    return data.json();
-}).then((data)=>{
-    console.log(data);
-}).catch((err)=>{
-    console.log(err);
-})
+// fetch(url).then((data)=>{
+//     return data.json();
+// }).then((data)=>{
+//     console.log(data);
+// }).catch((err)=>{
+//     console.log(err);
+// })
 
 async function getData(){
     let data=await fetch(url);
     let resultData=await data.json();
+    console.log(resultData);
     // data.json();
 
 }
+getData();
