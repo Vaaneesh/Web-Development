564d7b5d13a8eb6ab56b1be881a1f746754003d6
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-09-21 17:24:18 +0530

    changes made

--- Axios/script.js
+++ Axios/script.js
@@ -1,4 +1,11 @@
 let url="https://jsonplaceholder.typicode.com/todos/1";
-axios.get(url).then((response) => {
+// axios.get(url).then((response) => {
+//     console.log(response.data);
+// })
+
+//async
+async function myTask(){
+    let response=await axios.get(url)
     console.log(response.data);
-})
+}
+myTask();
