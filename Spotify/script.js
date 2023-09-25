95e581ba056a6b90a87f25514a06c08f179f4afb
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-09-25 16:44:58 +0530

    files modified

---
+++ Spotify/script.js
@@ -0,0 +1,94 @@
+//initializing
+let songIndex=0;
+let audioElement=new Audio('songs/1.mp3');
+let masterPlay=document.getElementById('masterplay');
+let myProgressbar=document.getElementById('myProgressBar');
+let gif=document.getElementById('gif');
+let songItems=Array.from(document.getElementsByClassName('songItem'));
+let songs=[
+    {songName:"The Hills",filePath:"songs/1.mp3",coverPath:"songList/L1.avif"},
+    {songName:"Starboy",filePath:"songs/2.mp3",coverPath:"songList/L2.jpeg"},
+    {songName:"Creepin'",filePath:"songs/3.mp3",coverPath:"songList/L3.jpeg"}
+]
+
+songItems.forEach((element,i)=>{
+    element.getElementsByTagName("img")[0].src=songs[i].coverPath;
+    element.getElementsByClassName("songName")[0].innerText=songs[i].songName;
+})
+// audioElement.play();
+
+//play/pause
+masterPlay.addEventListener('click',()=>{
+    if(audioElement.paused|| audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity=1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity=0;
+    }
+})
+
+//listen
+audioElement.addEventListener('timeupdate',()=>{
+    //updating seekbar
+    progress=parseInt((audioElement.currentTime/audioElement.duration)*100)
+    myProgressbar.value=progress;
+})
+
+myProgressbar.addEventListener('change',()=>{
+    audioElement.currentTime=(myProgressbar.value*audioElement.duration)/100;
+})
+
+const makeAllplays= ()=>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+        element.classList.remove('fa-pause-circle');
+        element.classList.add('fa-play-circle');
+
+    })
+}   
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.addEventListener('click',(e)=>{
+        makeAllplays();
+        songIndex=parseInt(e.target.id);
+        e.target.classList.remove('fa-play-circle');
+        e.target.classList.add('fa-pause-circle');
+        audioElement.src=`songs/${songIndex+1}.mp3`;
+        audioElement.currentTime=0;
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+    })
+})
+
+document.getElementsByid('next').addEventListener('click',()=>{
+    if(songIndex>=2){
+        songIndex=0;
+    }
+    else{
+        songIndex+=1;
+    }
+    audioElement.src=`songs/${songIndex+1}.mp3`;
+    audioElement.currentTime=0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+})
+document.getElementsByid('previous').addEventListener('click',()=>{
+    if(songIndex<=0){
+        songIndex=0;
+    }
+    else{
+        songIndex-=1;
+    }
+    audioElement.src='songs/${songIndex+1}.mp3';
+    audioElement.currentTime=0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+})
