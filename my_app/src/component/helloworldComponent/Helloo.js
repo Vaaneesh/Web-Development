891d49454c8f60d9360532986ed5eae5545f1a15
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-10-05 18:00:43 +0530

    changes made

---
+++ my_app/src/component/helloworldComponent/Helloo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Helloo = () => {
+  return (
+    <div>
+      <h1>Hello react</h1>
+    </div>
+  )
+}
+
+export default Helloo
---
+++ my_app/src/component/helloworldComponent/Helloworld.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Helloworld = () => {
+  return (
+    <div>
+      <h1>Hello React</h1>
+    </div>
+  )
+}
+
+
+export default Helloworld
