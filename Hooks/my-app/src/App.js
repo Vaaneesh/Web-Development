ff6d07989f1e4f402518646d6fec29d2ad057594
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-10-13 17:49:43 +0530

    changes done

--- Hooks/my-app/src/App.js
+++ Hooks/my-app/src/App.js
@@ -1,12 +1,20 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 function App() {
   // let searchInput="Hello";
   let [searchInput,setSearchInput]= useState("yo");
+  let searchref=useRef();
+  //setSearchInput is a method/function 
+  // let [value,setvalue]=useState(0);
+  // let changeValue=()=>{
+  //   setvalue(value+1);
+  // }
   return (
     <div>
-      <input type="text" value={searchInput} onChange={(ev)=>{
-        searchInput=ev.target.value;
+      {/* // <button onClick={changeValue}>Click Me!</button>
+      // <h1>{value}</h1> */}
+       <input  ref={searchref} type="text" value={searchInput} onChange={(ev)=>{
+        setSearchInput(ev.target.value);        
       }}></input>
       <h1>Learning Hooks</h1>
       <h2>{searchInput}</h2>
