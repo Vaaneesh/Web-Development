dce33b4f58dfdebf290957c80f9cd9005d8cbe35
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-10-13 16:57:22 +0530

    Files added

---
+++ Hooks/my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  let searchInput="Hello";
+  return (
+    <div>
+      <input type="text" value={searchInput}></input>
+      <h1>Learning Hooks</h1>
+    </div>
+  )
+}
+
+export default App
