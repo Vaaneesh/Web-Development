23af0ddb0d1f6477776a6baab5a5faf3e9d5a96d
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-10-26 13:24:54 +0530

    changes made

--- project/src/App.js
+++ project/src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
-import React,{useState} from "react";
+import React,{useState, useEffect} from "react";
 import Header from './components/Header';
 import Form from './components/Form';
 import Todolist from './components/Todolist';
 const App=()=> {
+  const initialState=JSON.parse(localStorage.getItem("todos"))|| [];
   const[input,setInput]=useState("");
-  const[todos,setTodos]=useState([]);
+  const[todos,setTodos]=useState(initialState);
   const[editTodo,setEditTodo]=useState(null);
-  return(<div className="container">
+ 
+  useEffect(()=>{
+    localStorage.setItem("todos",JSON.stringify(todos));
+  },[todos]);
+
+ return(<div className="container">
     <div className="app-wrapper">
       <div>
         <Header/>
--- project/src/components/Todolist.js
+++ project/src/components/Todolist.js
@@ -11,28 +11,56 @@ const Todolist = ({todos,setTodos,setEditTodo}) => {
             })
         )
     }
+    const handleEdit=({id})=>{
+        const findTodo=todos.find((todo)=>todo.id===id);
+        setEditTodo(findTodo);
+    }
     const handleDelete=({id})=>{
         setTodos(todos.filter((todo)=>todo.id!==id));
     };
+    const moveItemUp = (index) => {
+        if (index > 0) {
+          const updatedTodos = [...todos];
+          const temp = updatedTodos[index];
+          updatedTodos[index] = updatedTodos[index - 1];
+          updatedTodos[index - 1] = temp;
+          setTodos(updatedTodos);
+        }
+      };
+    
+      const moveItemDown = (index) => {
+        if (index < todos.length - 1) {
+          const updatedTodos = [...todos];
+          const temp = updatedTodos[index];
+          updatedTodos[index] = updatedTodos[index + 1];
+          updatedTodos[index + 1] = temp;
+          setTodos(updatedTodos);
+        }
+    }
   return (
     <div>
-      {todos.map((todo)=>(
+      {todos.map((todo,index)=>(
         <li className="list-item" key={todo.id}>
             <input
             type="text"
             value={todo.title}
-            className="list" onChange={(event)=>event.preventDefault()}
+            className={`list ${todo.completed ? "complete":""}`} 
+            onChange={(event)=>event.preventDefault()}
             />
             <div>
             <button className="button-complete task-button" onClick={()=>handleComplete(todo)}>
             <i className="fa fa-check-circle"></i>
             </button>
-            <button className="button-edit task-button">
+            <button className="button-edit task-button" onClick={()=>handleEdit(todo)}>
             <i className="fa fa-edit"></i>
             </button>
             <button className="button-delete task-button" onClick={()=>handleDelete(todo)}>
             <i className="fa fa-trash"></i>
             </button>
+            <button class="button-up task-button" onClick={()=>moveItemUp(index)}>↑</button>
+            <button class="button-down task-button" onClick={()=>moveItemDown(index)}>↓</button>
+
+
         </div>    
         </li>
       ))}
@@ -41,4 +69,4 @@ const Todolist = ({todos,setTodos,setEditTodo}) => {
   )
 }
 
-export default Todolist
+export default Todolist;
